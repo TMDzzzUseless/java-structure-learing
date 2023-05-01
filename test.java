7fc5e623e2b7e62c2fffd4f3fae2efc0a7960697
TMDzzzUseless <72007594+TMDzzzUseless@users.noreply.github.com>
2023-05-01 15:59:17 +0800

    Update test.java

--- test.java
+++ test.java
@@ -1,1 +1,15 @@
-
+//環境jdoodle
+import java.util.ArrayList; // import the ArrayList class
+public class MyClass {
+    public static void main(String args[]) {
+      ArrayList z= new ArrayList();
+      z.add(10);
+      System.out.println("ArrayList: "+z);
+      int n=0;
+      for(int i=3;i>0;i--){
+          n=n+1;
+          System.out.println(n);
+      }
+        
+    }
+}
