4691ce9c3ce7f2f1cdf98fd1f2901cee7c0b3730
TMDzzzUseless <72007594+TMDzzzUseless@users.noreply.github.com>
2023-09-22 11:41:39 +0800

    Update Recursion.js

--- Recursion.js
+++ Recursion.js
@@ -1,4 +1,5 @@
 public class Recursion {
+    //費波那切數列[1,1,2,3,5,8,13....]
     public static int fibo(int n){
       //這個是n==0或n==1
       if(n==0||n==1) 
@@ -8,5 +9,17 @@ public class Recursion {
     }
     public static void main(String args[]) {
       System.out.println(fibo(10));
+    }
+
+    //連續乘積[ans=1*3*5*7*...*(2n-3)*(2n-1)]
+    public static int s(int n){
+      //這個是n==0或n==1
+      if(n==0||n==1) 
+            return n;
+        else
+            return (2*n-1)*s(n-1);
+    }
+    public static void main(String args[]) {
+      System.out.println(s(4));
     }
 }
